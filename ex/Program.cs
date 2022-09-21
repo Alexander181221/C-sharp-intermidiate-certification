2b8cb80750acc43fc06c84df95047df1eb3cb8e0
Alexander <jayero@mail.ru>
2022-09-21 09:21:45 +0300

    создали метод, который проверят длину строк

---
+++ ex/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Задайте число строк");
+int count = Convert.ToInt32(Console.ReadLine());
+string [] array = new string [count];
+
+string [] GreaterOrLesserValue (string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+        
+    }
+ 
+ string [] result = new string[count];
+ int j = 0;
+
+ for (int i = 0; i < array.Length; i++)
+ {
+    if (array[i].Length <= 3)
+        {
+            result[j] = array[i];
+            j++;
+        }
+ }
+
+}
+
