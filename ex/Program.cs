f0723249bd5fc8eb48b0061a5ae5282cab67f7dd
Alexander <jayero@mail.ru>
2022-09-21 09:25:45 +0300

    создали метод, который заполняет массив

--- ex/Program.cs
+++ ex/Program.cs
@@ -1,8 +1,18 @@
 ﻿Console.WriteLine("Задайте число строк");
 int count = Convert.ToInt32(Console.ReadLine());
-string [] array = new string [count];
+string[] array = new string[count];
 
-string [] GreaterOrLesserValue (string [] array)
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string input = Console.ReadLine();
+        arr[i] = input;
+    }
+}
+
+
+string[] GreaterOrLesserValue(string[] array)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
@@ -11,20 +21,20 @@ string [] GreaterOrLesserValue (string [] array)
         {
             count++;
         }
-        
-    }
- 
- string [] result = new string[count];
- int j = 0;
 
- for (int i = 0; i < array.Length; i++)
- {
-    if (array[i].Length <= 3)
+    }
+
+    string[] result = new string[count];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
         {
             result[j] = array[i];
             j++;
         }
- }
+    }
+    return result;
 
 }
-
